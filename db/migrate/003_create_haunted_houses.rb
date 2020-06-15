24a68eb9a0a73c82e8c6e34b2083cc5b5240d1d8
jbaxter6 <jbaxter6@radford.edu>
2020-06-15 00:45:17 -0400

    Createing tables with migrations

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,17 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+class CreateHauntedHouses < ActiveRecord::Migration[6.0]
+    def change
+        create_table :haunted_houses do |t|
+        t.string :name
+        t.string :location
+        t.string :theme
+        t.integer :price
+        t.boolean :is_family
+        t.string :open_time
+        t.string :close_time
+        t.string :description
+        end
+    end
+end
